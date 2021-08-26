aebef9ed2d01ac85526de60433d156d687fb3a2b
Kucukcollu <kucukcollu@outlook.com>
2021-08-26 16:49:22 +0300

    Algorithm reconfig

--- service.py
+++ service.py
@@ -97,25 +97,6 @@ def detection():
             pass
 
 
-def gen_frames():  # generate frame by frame from camera
-    global out, capture, rec_frame
-    while True:
-        success, frame = camera.read()
-        if success:
-
-            try:
-                frame = cv2.rotate(frame, cv2.ROTATE_180)
-                ret, buffer = cv2.imencode('.jpg', cv2.flip(frame, -1))
-                frame = buffer.tobytes()
-                yield (b'--frame\r\n'
-                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-            except Exception as e:
-                pass
-
-        else:
-            pass
-
-
 @app.route("/")
 def index():
     return render_template('source.html')
@@ -124,12 +105,6 @@ def index():
 @app.route("/video_feed")
 def video_feed():
     return Response(detection(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-
-
-@app.route("/video_feed_original")
-def video_feed_original():
-    return Response(gen_frames(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
